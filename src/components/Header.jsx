4bcb79151e1fc13c02a242f4d1d964e94b82f457
김용민 <dydals3440@gmail.com>
2024-04-09 22:53:30 +0900

    feat: mouse hover 애니메이션으로 bounce 효과 주기

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { AnimatePresence } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 
 import NewChallenge from './NewChallenge.jsx';
 
@@ -21,9 +21,17 @@ export default function Header() {
       </AnimatePresence>
       <header id='main-header'>
         <h1>Your Challenges</h1>
-        <button onClick={handleStartAddNewChallenge} className='button'>
+        <motion.button
+          // onHoverStart={{ scale: 1.3 }}
+          // onHoverEnd={{ scale: 0.9 }}
+          whileHover={{ scale: 1.1 }}
+          // stiffness(빨라짐), mass(느려짐)
+          transition={{ type: 'spring', stiffness: 500 }}
+          onClick={handleStartAddNewChallenge}
+          className='button'
+        >
           Add Challenge
-        </button>
+        </motion.button>
       </header>
     </>
   );
