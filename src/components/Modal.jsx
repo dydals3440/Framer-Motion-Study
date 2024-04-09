9942f3cebbbb537b85afe72eafbf1353d0b3bb25
김용민 <dydals3440@gmail.com>
2024-04-09 22:43:18 +0900

    feat:  요소가 DOM에서 사라지는 요소를 AnimatePresence를 통해 해결

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,13 +1,22 @@
 import { createPortal } from 'react-dom';
+import { motion } from 'framer-motion';
 
 export default function Modal({ title, children, onClose }) {
   return createPortal(
     <>
-      <div className="backdrop" onClick={onClose} />
-      <dialog open className="modal">
+      <div className='backdrop' onClick={onClose} />
+      <motion.dialog
+        // initial (DOM에 추가 된후 곧바로 실행할 애니메이션의 초기 상태 적용 가능)
+        // 만약 그 상태가 animate 상태에 벗어나면 framer 모션이 자동으로 애니메이션을 재생해 목표로하는 animate 상태에 도달한다.
+        initial={{ opacity: 0, y: 30 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: 30 }}
+        open
+        className='modal'
+      >
         <h2>{title}</h2>
         {children}
-      </dialog>
+      </motion.dialog>
     </>,
     document.getElementById('modal')
   );
